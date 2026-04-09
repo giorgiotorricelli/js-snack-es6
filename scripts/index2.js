d26885a7611ec6eddc7c9a8287a4efa2100dccb8
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-04-09 17:51:26 +0200

    snack 2 completato

--- scripts/index2.js
+++ scripts/index2.js
@@ -1,1 +1,40 @@
-console.log("ciao");
+// Snack 2
+
+const nomiSquadre = ["Milan", "Inter", "Juventus", "Como", "Napoli", //nomi di squadre
+        "Roma", "Atalanta", "Bologna", "Lazio", "Sassuolo"];
+
+const teams = [];
+const arrayNomiEFalli = [];
+
+function creaObjects(squadre) { //funzione che prende l'array dei nomi delle squadre
+
+
+    for (let i = 0; i < squadre.length; i++) {
+
+        const puntiFatti = Math.ceil(Math.random() * 30); //scelta casuale dei punti
+        const falliSubiti = Math.ceil(Math.random() * 20); //scelta casuale falli 
+
+        const team = {
+            nome: nomiSquadre[i],
+            punti: puntiFatti,
+            falli: falliSubiti
+        }
+
+        teams.push(team);
+    }
+
+    console.log(teams);
+
+}
+
+creaObjects(nomiSquadre);
+
+
+for (let i = 0; i < teams.length; i++) {
+    arrayNomiEFalli.push(`Il team ${teams[i].nome} ha subito ${teams[i].falli} falli fino ad ora`);
+    console.log(arrayNomiEFalli[i]);
+    
+}
+
+
+
